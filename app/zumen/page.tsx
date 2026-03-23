90cf6fafaa3bbf732c8b043df25c0052cab3d84d
powerwayhouse1008 <info@powerway.jp>
2026-03-23 10:36:31 +0900

    Update page.tsx

--- app/zumen/page.tsx
+++ app/zumen/page.tsx
@@ -164,6 +164,8 @@ type ZumenData = {
   imgSub1?: string;
   imgSub2?: string;
   imgSub3?: string;
+  imgSub4?: string;
+  imgSub5?: string;
   imgQr?: string;
   imgMap?: string;
   draftTitle?: string;
@@ -1819,8 +1821,8 @@ function ZumenPageContent() {
               <div className="border-r border-black p-2">
                 <ImgBox src={data.imgPlan} label="間取り図（中央上）" h={360} fit="contain" />
                 <div className="mt-2 grid grid-cols-2 gap-2">
-                  <ImgBox src={data.imgSub2} label="室内（中央下左）" h={168} />
-                  <ImgBox src={data.imgSub3} label="共用（中央下右）" h={168} />
+                   <ImgBox src={data.imgSub4} label="リビング（中央下左）" h={168} />
+                  <ImgBox src={data.imgSub5} label="キッチン（中央下右）" h={168} />
                 </div>
               </div>
 
