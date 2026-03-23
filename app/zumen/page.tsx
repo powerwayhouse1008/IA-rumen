5bb940694211a9e2bfdb25aae2652eaddfbf3c3b
powerwayhouse1008 <info@powerway.jp>
2026-03-23 10:59:21 +0900

    Update page.tsx

--- app/zumen/page.tsx
+++ app/zumen/page.tsx
@@ -1864,8 +1864,8 @@ function ZumenPageContent() {
                     className={`whitespace-pre-wrap border border-black border-t-0 p-2 text-[10px] ${
                       template === "chic"
                         ? isMansion
-                          ? "min-h-[6.2cm]"
-                          : "min-h-[7.14cm]"
+                          ? "min-h-[6.21cm]"
+                          : "min-h-[7.15cm]"
                         : "min-h-[3.54cm]"
                     }`}
                   >
